39572df43d5501419577bda35c68ed242d87ea01
J Dark <james.dark.95@gmail.com>
2023-09-26 13:45:36 +0200

    typos

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 conda install -y -c conda-forge mamba
-mamba install -y -c conda-force fenics
-pip install festim==0.11.1 matplotlib==3.7.1 meshio[all] ipykernal pyparsing numpy==1.24
+mamba install -y -c conda-forge fenics
+pip install festim==0.11.1 matplotlib==3.7.1 meshio[all] ipykernel pyparsing numpy==1.24
